3c8da5ffa90682ec60f6b5032fdb8bba9e635ad4
unknown <riqtoriq@gmail.com>
2024-08-12 10:53:44 +0700

    [THOR] menambahkan script php

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Awal</title>
 </head>
+
 <body>
-    
+    <?php
+    if (1) {
+        echo 'hello';
+    }
+
+    ?>
 </body>
+
 </html>
